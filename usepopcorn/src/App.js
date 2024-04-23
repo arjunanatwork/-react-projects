c165df0d5e9af1fae61e79546050bffe61b5971b
Arjunan Ravi <arjunanatwork@gmail.com>
2024-04-23 19:09:53 +0400

    Add movie data fetching and loading state
    Imported useEffect alongside useState in App.js. Added loading state to fetch movie data from API call and updated the MovieList to display a Loader component while data is being fetched. This ensures that the page doesn't remain blank while waiting for response from API.

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -53,6 +53,23 @@ const average = (arr) =>
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchMovies() {
+      try {
+        setIsLoading(true);
+        const res = await fetch("http://www.omdbapi.com/?apiKey=f84fc31d&s=interstellar");
+        if (!res.ok) throw new Error("Failed to fetchMovies");
+        const data = await res.json();
+        setMovies(data.Search);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchMovies();
+  }, []);
 
   return (
     <>
@@ -62,7 +79,7 @@ export default function App() {
       </NavBar>
       <Main>
         <Box>
-          <MovieList movies={movies} />
+          {isLoading? <Loader /> : <MovieList movies={movies} />}
         </Box>
         <Box>
           <WatchedSummary watched={watched} />
@@ -71,6 +88,10 @@ export default function App() {
       </Main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="loader">Loading..</p>
 }
 
 function NavBar({ children }) {
