89c534080674f6ce1ee5d26aa56336c4dfe74838
Arjunan Ravi <arjunanatwork@gmail.com>
2024-05-05 10:51:27 +0400

    Add Progress and NextButton components and update Questions and Options
    The Progress component for displaying the progress of the quiz and the NextButton component to navigate to the next question were added. The Question and Options components were modified to accommodate changes for answer handling and state management where necessary with the 'nextQuestion' and 'newAnswer' actions dispatched from these components.

--- react-quiz/src/components/App.js
+++ react-quiz/src/components/App.js
@@ -5,11 +5,15 @@ import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
+import NextButton from "./NextButton";
+import Progress from "./Progress";
 
 const initialState = {
     questions: [],
     status: 'loading',
-    index: 0
+    index: 0,
+    answer: null,
+    points: 0
 }
 
 function reducer(state = initialState, action) {
@@ -20,6 +24,14 @@ function reducer(state = initialState, action) {
             return {...state, status: 'error'};
         case 'start':
             return {...state, status: 'active'};
+        case 'newAnswer':
+            const question = state.questions.at(state.index);
+            return {
+                ...state, answer: action.payload, points: action.payload === question.correctOption
+                    ? state.points + question.points : state.points
+            };
+        case 'nextQuestion':
+            return {...state, index: state.index + 1, answer: null};
         default:
             throw new Error(`Unhandled action type ${action.type}`);
     }
@@ -28,10 +40,10 @@ function reducer(state = initialState, action) {
 
 export default function App() {
 
-    const [{questions, status, index}, dispatch] = useReducer(reducer, initialState);
+    const [{questions, status, index, answer, points}, dispatch] = useReducer(reducer, initialState);
 
     const numQuestions = questions.length;
-
+    const maxPossiblePoints = questions.reduce((prev, curr) => prev + curr.points, 0);
     useEffect(() => {
         fetch("http://localhost:9000/questions")
             .then((res) => res.json())
@@ -45,7 +57,15 @@ export default function App() {
             {status === 'loading' && <Loader/>}
             {status === 'error' && <Error/>}
             {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch}/>}
-            {status === 'active' && <Question question={questions[index]}/>}
+            {status === 'active' && (
+                <>
+                    <Progress numQuestions={numQuestions} index={index} points={points}
+                              maxPossiblePoints={maxPossiblePoints} answer={answer}/>
+                    <Question question={questions[index]} dispatch={dispatch} answer={answer}/>
+                    <NextButton dispatch={dispatch} answer={answer}/>
+                </>
+            )
+            }
         </Main>
     </div>);
 }
