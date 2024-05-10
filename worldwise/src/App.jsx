cd19085e46cfbee130b215e3cdd36993f56258b6
Arjunan Ravi <arjunanatwork@gmail.com>
2024-05-10 16:58:24 +0400

    Add basic routing and new pages
    Routing functionality has been added to the "App" component using "react-router-dom". New pages including 'Homepage', 'Product', 'Pricing', and 'PageNotFound' are now included and accessible via the router. Additionally, "react-router-dom" was added as a dependency in package.json.

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,7 +1,19 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Product from "./pages/Product.jsx";
+import Homepage from "./pages/Homepage.jsx";
+import Pricing from "./pages/Pricing.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+
 function App() {
-    const x = 23
     return (
-        <div>Worldwise</div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Homepage/>}/>
+                <Route path="product" element={<Product/>}/>
+                <Route path="pricing" element={<Pricing/>}/>
+                <Route path="*" element={<PageNotFound/>}/>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
