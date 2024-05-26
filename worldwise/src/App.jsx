03a93de2374ffa1683fe0d9c81ba0d0d73313a7e
Arjunan Ravi <arjunanatwork@gmail.com>
2024-05-26 14:32:22 +0400

    Add CityList, CityItem, and CountryList components
    This commit introduces new components: CityList, CityItem, and CountryList. It also integrates fetching of city data using React's useEffect hook in the App component and displays content based on whether data is loading or not. The 'json-server' package is added to the dependencies in package.json to provide a fake REST API for development.

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -5,8 +5,33 @@ import Pricing from "./pages/Pricing.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
 import Login from "./pages/Login.jsx";
+import CityList from "./components/CityList.jsx";
+import {useEffect, useState} from "react";
+import CountryList from "./components/CountryList.jsx";
+
+const BASE_URL = 'http://localhost:8000';
 
 function App() {
+
+    const [cities, setCities] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        async function fetchCities() {
+            try {
+                const res = await fetch(`${BASE_URL}/cities`);
+                const data = await res.json();
+                setCities(data)
+            } catch {
+                alert("Failed to fetch cities.");
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        fetchCities();
+    }, []);
+
     return (
         <BrowserRouter>
             <Routes>
@@ -14,10 +39,10 @@ function App() {
                 <Route path="product" element={<Product/>}/>
                 <Route path="pricing" element={<Pricing/>}/>
                 <Route path="login" element={<Login/>}/>
-                <Route path="app" element={<AppLayout />}>
-                    <Route index element={<p>List of Cities</p>}/>
-                    <Route path="cities" element={<p>List of Cities</p>}/>
-                    <Route path="countries" element={<p>Countries</p>}/>
+                <Route path="app" element={<AppLayout/>}>
+                    <Route index element={<CityList cities={cities} isLoading={isLoading}/>}/>
+                    <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>}/>
+                    <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading}/>}/>
                     <Route path="form" element={<p>Form</p>}/>
                 </Route>
                 <Route path="*" element={<PageNotFound/>}/>
