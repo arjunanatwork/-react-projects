2c1d54ab727df460e7699760773f97c2d9e36f19
Arjunan Ravi <arjunanatwork@gmail.com>
2024-05-26 21:23:05 +0400

    Add React router navigation and update various components
    Enhanced the City, CountryList Form, and Map components to make use of React Router's navigation features. This allows us to direct the user to different views based on the application's URL. Also, updated the BrowserRouter import in App.jsx to include Navigate, and included routing for the City and Form components. As a result, we removed the use of hardcoded values in the City component and replaced it with dynamic routing parameters and search parameters for better flexibility. Also added a new Button component, and utilized it in the Form's back button, along with useNavigate hook.

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import Product from "./pages/Product.jsx";
 import Homepage from "./pages/Homepage.jsx";
 import Pricing from "./pages/Pricing.jsx";
@@ -8,6 +8,8 @@ import Login from "./pages/Login.jsx";
 import CityList from "./components/CityList.jsx";
 import {useEffect, useState} from "react";
 import CountryList from "./components/CountryList.jsx";
+import City from "./components/City.jsx";
+import Form from "./components/Form.jsx";
 
 const BASE_URL = 'http://localhost:8000';
 
@@ -40,10 +42,11 @@ function App() {
                 <Route path="pricing" element={<Pricing/>}/>
                 <Route path="login" element={<Login/>}/>
                 <Route path="app" element={<AppLayout/>}>
-                    <Route index element={<CityList cities={cities} isLoading={isLoading}/>}/>
+                    <Route index element={<Navigate replace to={'cities'}/>}/>
                     <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>}/>
+                    <Route path="cities/:id" element={<City/>}/>
                     <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading}/>}/>
-                    <Route path="form" element={<p>Form</p>}/>
+                    <Route path="form" element={<Form/>}/>
                 </Route>
                 <Route path="*" element={<PageNotFound/>}/>
             </Routes>
---
+++ worldwise/src/components/Button.jsx
@@ -0,0 +1,9 @@
+import styles from "./Button.module.css";
+
+function Button({children, onCLick, type}) {
+    return (
+        <button onClick={onCLick} className={`${styles.btn} ${styles[type]}`}>{children}</button>
+    )
+}
+
+export default Button;
