c65209860bb45c5416b773828f0f349ece3b423b
Arjunan Ravi <arjunanatwork@gmail.com>
2024-06-09 13:02:07 +0400

    Add geolocation and URL position hooks, map updates
    Two hooks have been added: useGeolocation and useUrlPosition to get the user's current location and the lat/long parameters from the URL, respectively. The map component now uses Leaflet to render a map with these coordinates, moving when the user clicks on new locations. Changes also include added dependencies for the Leaflet library, and updates to the Form component to handle these new geolocation and map features.

--- worldwise/src/components/Form.jsx
+++ worldwise/src/components/Form.jsx
@@ -1,10 +1,13 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 import styles from "./Form.module.css";
 import Button from "./Button.jsx";
 import BackButton from "./BackButton.jsx";
+import {useUrlPosition} from "../hooks/useUrlPosition.js";
+import Message from "./Message.jsx";
+import Spinner from "./Spinner.jsx";
 
 export function convertToEmoji(countryCode) {
     const codePoints = countryCode
@@ -14,11 +17,44 @@ export function convertToEmoji(countryCode) {
     return String.fromCodePoint(...codePoints);
 }
 
+const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client"
+
 function Form() {
+    const [lat, lng] = useUrlPosition();
 
     const [cityName, setCityName] = useState("");
     const [date, setDate] = useState(new Date());
     const [notes, setNotes] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [country, setCountry] = useState("");
+    const [emoji, setEmoji] = useState("");
+    const [geocodingError, setGeocodingError] = useState("");
+
+    useEffect(() => {
+        async function fetchCityData() {
+            try {
+                setIsLoading(true);
+                setGeocodingError("");
+                const res = await fetch(`${BASE_URL}?latitude=${lat}&longitude=${lng}`);
+                const data = await res.json();
+                console.log(data);
+                if (!data.countryCode) throw new Error("That doesn't seem to be a city 🤔")
+                setCityName(data.city || data.locality || "")
+                setCountry(data.countryName);
+                setEmoji(convertToEmoji(data.countryCode))
+            } catch (err) {
+                setGeocodingError(err.message);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        fetchCityData();
+    }, [lat, lng])
+
+    if (isLoading) return <Spinner/>;
+
+    if (geocodingError) return <Message message={geocodingError}/>;
 
     return (
         <form className={styles.form}>
@@ -29,7 +65,7 @@ function Form() {
                     onChange={(e) => setCityName(e.target.value)}
                     value={cityName}
                 />
-                {/* <span className={styles.flag}>{emoji}</span> */}
+                {<span className={styles.flag}>{emoji}</span>}
             </div>
 
             <div className={styles.row}>
