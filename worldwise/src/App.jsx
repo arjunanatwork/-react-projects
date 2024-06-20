045e7183f9e3689be1bfdc060d2b0d40bfa34850
Arjunan Ravi <arjunanatwork@gmail.com>
2024-06-20 12:01:14 +0400

    Implement lazy loading for page components
    The modifications in this commit are focused on improving the application's performance by implementing lazy loading for the page components. The changes include importing the React Suspense and lazy functionality, defined with the loading of each page component. The application will display a full page spinner while page components are loading.

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,10 +1,10 @@
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
-import Product from "./pages/Product.jsx";
-import Homepage from "./pages/Homepage.jsx";
-import Pricing from "./pages/Pricing.jsx";
-import PageNotFound from "./pages/PageNotFound.jsx";
-import AppLayout from "./pages/AppLayout.jsx";
-import Login from "./pages/Login.jsx";
+// import Product from "./pages/Product.jsx";
+// import Homepage from "./pages/Homepage.jsx";
+// import Pricing from "./pages/Pricing.jsx";
+// import PageNotFound from "./pages/PageNotFound.jsx";
+// import AppLayout from "./pages/AppLayout.jsx";
+// import Login from "./pages/Login.jsx";
 import CityList from "./components/CityList.jsx";
 import CountryList from "./components/CountryList.jsx";
 import City from "./components/City.jsx";
@@ -12,6 +12,15 @@ import Form from "./components/Form.jsx";
 import {CitiesProvider} from "./contexts/CitiesContext.jsx";
 import {AuthProvider} from "./contexts/FakeAuthContext.jsx";
 import ProtectedRoute from "./pages/ProtectedRoute.jsx";
+import {lazy, Suspense} from "react";
+import SpinnerFullPage from "./components/SpinnerFullPage.jsx";
+
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Login = lazy(() => import("./pages/Login"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 
 
 function App() {
@@ -19,20 +28,22 @@ function App() {
         <AuthProvider>
             <CitiesProvider>
                 <BrowserRouter>
-                    <Routes>
-                        <Route index element={<Homepage/>}/>
-                        <Route path="product" element={<Product/>}/>
-                        <Route path="pricing" element={<Pricing/>}/>
-                        <Route path="login" element={<Login/>}/>
-                        <Route path="app" element={<ProtectedRoute><AppLayout/></ProtectedRoute>}>
-                            <Route index element={<Navigate replace to={'cities'}/>}/>
-                            <Route path="cities" element={<CityList/>}/>
-                            <Route path="cities/:id" element={<City/>}/>
-                            <Route path="countries" element={<CountryList/>}/>
-                            <Route path="form" element={<Form/>}/>
-                        </Route>
-                        <Route path="*" element={<PageNotFound/>}/>
-                    </Routes>
+                    <Suspense fallback={<SpinnerFullPage/>}>
+                        <Routes>
+                            <Route index element={<Homepage/>}/>
+                            <Route path="product" element={<Product/>}/>
+                            <Route path="pricing" element={<Pricing/>}/>
+                            <Route path="login" element={<Login/>}/>
+                            <Route path="app" element={<ProtectedRoute><AppLayout/></ProtectedRoute>}>
+                                <Route index element={<Navigate replace to={'cities'}/>}/>
+                                <Route path="cities" element={<CityList/>}/>
+                                <Route path="cities/:id" element={<City/>}/>
+                                <Route path="countries" element={<CountryList/>}/>
+                                <Route path="form" element={<Form/>}/>
+                            </Route>
+                            <Route path="*" element={<PageNotFound/>}/>
+                        </Routes>
+                    </Suspense>
                 </BrowserRouter>
             </CitiesProvider>
         </AuthProvider>
