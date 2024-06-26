b387df17963e5fabfdcbe88ad0faa4eb8deb2eab
Arjunan Ravi <arjunanatwork@gmail.com>
2024-06-26 17:39:56 +0400

    Add core features for fast-react-pizza application
    This commit delivers essential functionality for fast-react-pizza application. It involves the implementation of services, hooks, components, and state handlers for features such as menu display, cart overview, order creation, and user handling. The commit includes error handling, form validation, and user interface components providing standard operations like fetching geolocation and menu from API, displaying cart items, and updating orders.

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,5 +1,45 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from "./ui/Home.jsx";
+import Menu, {loader as menuLoader} from "./features/menu/Menu.jsx";
+import Cart from "./features/cart/Cart.jsx";
+import CreateOrder from "./features/order/CreateOrder.jsx";
+import Order from "./features/order/Order.jsx";
+import AppLayout from "./ui/AppLayout.jsx";
+import Error from "./ui/Error.jsx";
+
+const router = createBrowserRouter([
+    {
+        element: <AppLayout/>,
+        errorElement: <Error/>,
+        children: [
+            {
+                path: "/",
+                element: <Home/>
+            },
+            {
+                path: "/menu",
+                element: <Menu/>,
+                loader: menuLoader,
+                errorElement: <Error/>
+            },
+            {
+                path: "/cart",
+                element: <Cart/>
+            },
+            {
+                path: "/order/new",
+                element: <CreateOrder/>
+            },
+            {
+                path: "/order/:orderId",
+                element: <Order/>
+            }
+        ]
+    }
+]);
+
 function App() {
-    return <div>Hello Vite</div>
+    return <RouterProvider router={router}/>;
 }
 
 export default App;
