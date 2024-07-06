9d132c2b2dc17506ef10e63d26bdbac66a2dc427
Arjunan Ravi <arjunanatwork@gmail.com>
2024-07-06 06:47:26 +0400

    feat: Update order handling and implement search feature
    This update modifies the order handling in Order and CreateOrder to use server responses fetched through API calls. Additionally, an order search feature has been added in a new file, SearchOrder. The aim is to enhance the functionality and user interactivity of the ordering process by providing real time data from the server and adding a search option for orders.

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -2,8 +2,8 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Home from "./ui/Home.jsx";
 import Menu, {loader as menuLoader} from "./features/menu/Menu.jsx";
 import Cart from "./features/cart/Cart.jsx";
-import CreateOrder from "./features/order/CreateOrder.jsx";
-import Order from "./features/order/Order.jsx";
+import CreateOrder, {action as createOrderAction} from "./features/order/CreateOrder.jsx";
+import Order, {loader as orderLoader} from "./features/order/Order.jsx";
 import AppLayout from "./ui/AppLayout.jsx";
 import Error from "./ui/Error.jsx";
 
@@ -28,11 +28,14 @@ const router = createBrowserRouter([
             },
             {
                 path: "/order/new",
-                element: <CreateOrder/>
+                element: <CreateOrder/>,
+                action: createOrderAction
             },
             {
                 path: "/order/:orderId",
-                element: <Order/>
+                element: <Order/>,
+                loader: orderLoader,
+                errorElement: <Error/>
             }
         ]
     }
