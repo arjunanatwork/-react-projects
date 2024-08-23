e95be2079e3d55fed06841c3017ea049d53c6698
Arjunan Ravi <arjunanatwork@gmail.com>
2024-08-23 19:33:58 +0400

    Enhance booking and cabin management features
    Added useBookings custom hook for handling feature-specific use cases and integrated it into BookingTable. BookingDetail component now fetches data using useBooking hook, displaying booking details with a fallback Spinner. Enhanced CabinTable with sorting and empty state handling. Added server-side pagination to BookingTable and created reusable Pagination component. Introduced new utility constants and components like Select and SortBy for better state management and UI consistency.

--- the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -4,12 +4,14 @@ import { useCabins } from './useCabins.js';
 import Table from '../../ui/Table.jsx';
 import Menus from '../../ui/Menus.jsx';
 import { useSearchParams } from 'react-router-dom';
+import Empty from '../../ui/Empty.jsx';
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
   const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+  if (!cabins.length) return <Empty resource='cabins' />;
 
   const filterValue = searchParams.get('discount') || 'all';
 
@@ -19,6 +21,11 @@ function CabinTable() {
     filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
   if (filterValue === 'with-discount')
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+  const sortBy = searchParams.get('sortBy') || 'startDate-asc';
+  const [field, direction] = sortBy.split('-');
+  const modifier = direction === 'asc' ? 1 : -1;
+  const sortedCabins = filteredCabins.sort((a, b) => (a[field] - b[field]) * modifier);
 
   return (
     <Menus>
@@ -32,7 +39,7 @@ function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
         />
       </Table>
